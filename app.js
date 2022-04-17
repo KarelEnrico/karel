a84ecf4fd817ac4d75b35b1bff0900eadca358d1
Karl <94813940+KarelEnrico@users.noreply.github.com>
2022-04-17 13:05:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Karel Enrico (52019014) TI \n')
 })
 
 app.listen(PORT, HOST)
